17a002c4e08befc5562da4da10d9a84fc19cb76b
Samer Mobarak <samermobarak888@gmail.com>
2023-08-07 15:32:47 -0400

    control

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber > 0) {
+    result = 'This one is on me!'
+  }
+  if (someNumber > 400) {
+    result = 'That will be twenty bucks.'
+  }
+  if (someNumber > 2000) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  if (someNumber > 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    }
+  
+
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
